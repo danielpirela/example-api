a8cb04c827a9289193bedf3b4de88476c277987e
danielpirela <danielapirelac@gmail.com>
2023-06-18 19:38:28 -0400

    s

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,6 @@ app.get('/ping', (_req, res) => {
 
 app.use('/api/diaries', diaryRouter)
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
